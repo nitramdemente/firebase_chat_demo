af209ec6c446298e8c8f192781d75fd5b460e32f
x <x>
2016-11-28 09:47:21 -0400

    Initial commit

---
+++ app/src/main/java/demo/lgmg/firebasechat/firebase_chat_demo/MainActivity.java
@@ -0,0 +1,13 @@
+package demo.lgmg.firebasechat.firebase_chat_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
